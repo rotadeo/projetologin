6829d82fafc90179484fe3e5cc72b288513d3b67
Rodrigo <rodrigocardosotadeo@gmail.com>
2025-09-25 09:56:03 -0300

    .

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.post("/register", async(req,res)=>{
 app.post("/login", async (req,res)=>{
     const {email, senha}= req.body;
     const users = consultarUsuarios();
-    const user = user.find(user=>user.email === email);
+    const user = users.find(user=>user.email === email);
 
     if(!user){
         return res.status(400).json({message: "Usuario/senha inválidos"})
