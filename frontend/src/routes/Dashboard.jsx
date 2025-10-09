9751db4eaa4622900b1d3ace0052a7b686d590d0
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-09 09:31:08 -0300

    adicionando o dashboard

--- frontend/src/routes/Dashboard.jsx
+++ frontend/src/routes/Dashboard.jsx
@@ -1,29 +1,49 @@
-import React from 'react'
-import produtos from '../../../backend/data/produtos.json' // ajuste o caminho se estiver em outra pasta
+import {useNavigate} from "react-router-dom"
+import {Chart} from 'react-google-charts'
+import data from '../../../backend/data/produtos.json'
+
+//simulando os dados do banco
+
+  export const options={
+    isStacked:true,
+    height:300,
+    legend:{ position:"top", maxLines:3},
+    vAxis:{minValue:0}
+};
 
 const Dashboard = () => {
-  return (
-    <div className="min-h-screen bg-gray-300 p-8">
-      <h1 className="mt-8 text-3xl font-bold text-center text-gray-800 mb-8">
-        Seja Bem-Vindo ao Dashboard
-      </h1>
+  const navigate=useNavigate();
 
-      <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {produtos.map((produto) => (
-          <div
-            key={produto.id}
-            className="bg-white shadow-md rounded-lg p-6 hover:shadow-lg transition"
-          >
-            <h2 className="text-xl font-semibold text-gray-700 mb-2">
-              {produto.nome}
-            </h2>
-            <p className="text-gray-600">ID: {produto.id}</p>
-            <p className="text-blue-600 font-bold mt-2">
-              R$ {produto.valor.toFixed(2)}
-            </p>
-          </div>
-        ))}
-      </div>
+//  FUNÇÃO handleLogout
+const handleLogout=()=>{
+  localStorage.removeItem("token");
+  navigate("/")
+}
+
+  
+
+
+  return (
+    <div>
+      <h1>Seja Bem-vindo ao Dashboard</h1>
+      <button onClick={handleLogout} className=" mt-6 w-[100px] h-[60px] bg-blue-950 text-white rounded-2xl">
+        Logout
+      </button>
+
+      <Chart
+      chartType="AreaChart"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+
+      />
+
+
+
+
+
+
     </div>
   )
 }
